a6f52c33d872baaf8dd052c81d68316a43a1881a
Anders Wahlfridsson <agwahlfridsson@gmail.com>
2025-02-15 12:32:37 +0100

    Added numerictextbox

---
+++ NumericTextBox/UserControlLibrary/NumericTextBox.cs
@@ -0,0 +1,328 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UserControlLibrary
+{
+    public enum MouseWheelDeltaDirectionTypes
+    { 
+        UpIncrease, UpDecrease
+    }
+    
+    public class NumericTextBox : TextBox
+    {
+        private bool suppressUpdate = false;
+        #region Properties
+        public double DeltaMinor { get; set; } = 0.1;
+        public double DeltaMajor { get; set; } = 1.0;
+        public double Minimum { get; set; } = double.MinValue;
+        public double Maximum { get; set; } = double.MaxValue;
+        public int Decimals { get; set; } = 0;
+        public string DecimalSeparator { get; set; } = ".";
+        public bool EnableUpdateValueOutOfRange { get; set; } = true;
+        public bool EnableScrollWheelChange { get; set; } = true;
+        public MouseWheelDeltaDirectionTypes MouseWheelDeltaDirection { get; set; } = MouseWheelDeltaDirectionTypes.UpIncrease;
+        public string LatestEvent
+        {
+            get { return (string)GetValue(LatestEventProperty); }
+            set { SetValue(LatestEventProperty, value); }
+        }
+        public static readonly DependencyProperty LatestEventProperty =
+            DependencyProperty.Register("LatestEvent", typeof(string), typeof(NumericTextBox), new PropertyMetadata(""));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(NumericTextBox), new PropertyMetadata(double.NaN, OnValueUpdate));
+
+        private static void OnValueUpdate(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var tb = d as NumericTextBox;
+            if (tb == null) 
+                return;
+
+            var nfi = new NumberFormatInfo
+            {
+                NumberDecimalDigits = tb.Decimals,
+                NumberDecimalSeparator = tb.DecimalSeparator
+            };
+
+            var val = (double)tb.Value;
+            val = Math.Round(val, tb.Decimals);
+            tb.suppressUpdate = true;
+            tb.Text = string.Format(nfi, "{0:f}", val);
+            tb.CaretIndex = tb.Text.Length;
+
+        }
+
+        public bool InRange
+        {
+            get { return (bool)GetValue(InRangeProperty); }
+            set { SetValue(InRangeProperty, value); }
+        }
+        public static readonly DependencyProperty InRangeProperty =
+            DependencyProperty.Register("InRange", typeof(bool), typeof(NumericTextBox), new PropertyMetadata(false));
+        #endregion
+
+
+        public NumericTextBox()
+        {
+            this.PreviewKeyDown += NumericTextBox_PreviewKeyDown;
+            this.MouseWheel += NumericTextBox_MouseWheel;
+            this.TextChanged += NumericTextBox_TextChanged;
+        }
+
+        private void NumericTextBox_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Value == double.NaN) 
+                return;
+            if (EnableScrollWheelChange == false) 
+                return;   
+
+            if (e.Delta > 0)  // scroll up
+            {
+                if (MouseWheelDeltaDirection == MouseWheelDeltaDirectionTypes.UpIncrease)
+                {
+                    // increase
+                    if (Keyboard.Modifiers == ModifierKeys.Shift)
+                        SetValue(Value, DeltaMajor);
+                    else
+                        SetValue(Value, DeltaMinor);
+                }
+                else
+                {
+                    // decrease
+                    if (Keyboard.Modifiers == ModifierKeys.Shift)
+                        SetValue(Value, -DeltaMajor);
+                    else
+                        SetValue(Value, -DeltaMinor);
+                }
+            }
+            else // scroll down
+            {
+                if (MouseWheelDeltaDirection == MouseWheelDeltaDirectionTypes.UpDecrease)
+                {
+                    // increase
+                    if (Keyboard.Modifiers == ModifierKeys.Shift)
+                        SetValue(Value, DeltaMajor);
+                    else
+                        SetValue(Value, DeltaMinor);
+                }
+                else
+                {
+                    // decrease
+                    if (Keyboard.Modifiers == ModifierKeys.Shift)
+                        SetValue(Value, -DeltaMajor);
+                    else
+                        SetValue(Value, -DeltaMinor);
+                }
+            }
+        }
+
+        private void NumericTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            var tb = sender as TextBox;
+            if (tb == null)
+                return;
+
+            switch (e.Key)
+            {
+                case Key.Up: 
+                    {
+                        if (Decimals == 0 & DeltaMinor < 1) // no update 
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+
+                        SetValue(Value, DeltaMinor);
+                        e.Handled = true;
+                    } break;
+                case Key.Down: 
+                    {
+                        if (Decimals == 0 & DeltaMinor < 1) // no update 
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+
+                        SetValue(Value, -DeltaMinor);
+                        e.Handled = true;
+                    } break;
+                case Key.PageUp:
+                    {
+                        if (Decimals == 0 & DeltaMajor < 1) // no update 
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+
+                        SetValue(Value, DeltaMajor);
+                    } break;
+                case Key.PageDown: 
+                    {
+                        if (Decimals == 0 & DeltaMajor < 1) // no update 
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+                        
+                        SetValue(Value, -DeltaMajor);
+                    } break;
+                case Key.Delete: { } break;
+                case Key.Back: { } break;
+                case Key.OemMinus:
+                case Key.Subtract:
+                    {
+                        if (tb.CaretIndex != 0) // disable minus character if index is not the first
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+                        if (Minimum >= 0) // disable minus character if non negative values are allowed
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+                    } break;
+                case Key.Left:
+                    {
+                        if (this.CaretIndex > 0)
+                            this.CaretIndex -= 1;
+                    }break;
+                case Key.Right:
+                    { 
+                        if (this.CaretIndex < this.Text.Length)
+                            this.CaretIndex += 1;
+                    }
+                    break;
+                case Key.Home:
+                case Key.End:
+                case Key.LeftShift:
+                case Key.RightShift:
+                case Key.OemComma:
+                case Key.Decimal:
+                case Key.OemPeriod: 
+                    {
+                        if (Decimals <= 0) // disable period character if non float values are accepted
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+                        else if (Text.Contains(",") || Text.Contains(".")) // disable additional comma separators
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+                    } break;
+                case Key.NumPad0:
+                case Key.NumPad1:
+                case Key.NumPad2:
+                case Key.NumPad3:
+                case Key.NumPad4:
+                case Key.NumPad5:
+                case Key.NumPad6:
+                case Key.NumPad7:
+                case Key.NumPad8:
+                case Key.NumPad9:
+                case Key.D0:
+                case Key.D1:
+                case Key.D2:
+                case Key.D3:
+                case Key.D4:
+                case Key.D5:
+                case Key.D6:
+                case Key.D7:
+                case Key.D8:
+                case Key.D9:
+                    {
+                        // Limit amout of decimals 
+                        if (Decimals > 0)
+                        {
+                            var split = this.Text.Replace(",", ".").Split(".");
+                            if (split.Length > 1) // decimal part exist
+                            {
+                                var indexOfPeriod = Text.Replace(",", ".").IndexOf(".");
+                                var caretIsBehindPeriod = CaretIndex > indexOfPeriod;
+                                if (split[1].Length >= Decimals &&
+                                    caretIsBehindPeriod == true) // disable writing of more decimal characters
+                                {
+                                    e.Handled = true;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    {
+                        // ignore other keys!
+                        e.Handled = true;
+                    }
+                    break;
+            }
+        }
+
+        private void NumericTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (suppressUpdate == true)
+            {
+                suppressUpdate = false;
+                return;
+            }
+
+            if (double.TryParse(Text.Replace(",", "."), CultureInfo.InvariantCulture, out var d))
+            {
+                SetValue(d, 0);
+                //if (d >= Minimum && d <= Maximum) // in range
+                //{
+                //    this.Value = d;
+                //    this.InRange = true;
+                //}
+                //else
+                //{
+                //    this.InRange = false;
+                //    if (EnableUpdateValueOutOfRange == true)
+                //    { 
+                //        this.Value = d;
+                //    }
+                //}
+            }
+        }
+        private void SetValue(double newValue)
+        {
+            void Set(double value)
+            {
+                Value = Math.Round(newValue, Decimals);
+                InRange = Value <= Maximum && Value >= Minimum;
+            }
+            if (EnableUpdateValueOutOfRange == false) // cap values
+            {
+                if (newValue > Maximum) // set Value to maximun if over limit
+                {
+                    Set(Maximum);
+                }
+                else if (newValue < Minimum) // set Value to minimum if over limit
+                {
+                    Set(Minimum);
+                }
+                else
+                {
+                    Set(newValue);
+                }
+            }
+            else
+            {
+                Set(newValue);
+            }
+        }
+        private void SetValue(double newValue, double delta)
+        {
+            SetValue(newValue + delta);
+        }
+    }
+}
---
+++ NumericTextBox/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    public class CustomTextBox : TextBox 
+    {
+        public bool IsEditMode
+        {
+            get { return (bool)GetValue(IsEditModeProperty); }
+            set { SetValue(IsEditModeProperty, value); }
+        }
+        public static readonly DependencyProperty IsEditModeProperty =
+            DependencyProperty.Register("IsEditMode", typeof(bool), typeof(CustomTextBox), new PropertyMetadata(false));
+
+        public bool IsNumeric
+        {
+            get { return (bool)GetValue(IsNumericProperty); }
+            set { SetValue(IsNumericProperty, value); }
+        }
+        public static readonly DependencyProperty IsNumericProperty =
+            DependencyProperty.Register("IsNumeric", typeof(bool), typeof(CustomTextBox), new PropertyMetadata(false));
+
+        private string _orgText = string.Empty; 
+        public CustomTextBox()
+        {
+            this.KeyDown += (s, e) => 
+            {
+                if (e.Key == System.Windows.Input.Key.Enter && this.IsEditMode)
+                {
+                    _orgText = this.Text;
+                    this.IsEditMode = false;
+                    var be = this.GetBindingExpression(TextBox.TextProperty);
+                    be.UpdateSource();
+                }
+                else if (e.Key == System.Windows.Input.Key.Escape)
+                {
+                    this.Text = _orgText;
+                    this.IsEditMode = false;
+                    this.CaretIndex=this.Text.Length;
+                }
+                else
+                {
+                    if (this.IsEditMode == false) 
+                    {
+                        this.IsEditMode = true;
+                    }
+                    this.IsNumeric = double.TryParse(this.Text.Replace(',', '.'), CultureInfo.InvariantCulture, out double d);
+                }
+            };
+        }
+    }
+    public class MainWindowViewModel
+    {
+        private string _textValue = "";
+
+        public string TextValue
+        {
+            get { return _textValue; }
+            set 
+            {
+                _textValue = value; 
+            }
+        }
+
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
